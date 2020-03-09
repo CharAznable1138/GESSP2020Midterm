0549f0801adeee51985b4b080efb3b027509e08b
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-03-09 17:17:24 -0500

    implemented working score display

---
+++ Assets/Scripts/PlayerBulletCollisionManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBulletCollisionManager : MonoBehaviour
+{
+    private GameObject scoreDisplay;
+    private ScoreManager scoreManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreDisplay = GameObject.Find("ScoreDisplay");
+        scoreManager = scoreDisplay.GetComponent<ScoreManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+            //Debug.Log("Just killed an enemy");
+            scoreManager.Score++;
+        }
+        if (collision.gameObject.CompareTag("EnemyProjectile"))
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+            //Debug.Log("Two bullets just collided & destroyed each other");
+        }
+    }
+}
