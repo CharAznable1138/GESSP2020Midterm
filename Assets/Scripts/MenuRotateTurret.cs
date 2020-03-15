1260b78f3ef0110506952ff2c17e6ad18d364128
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-03-14 20:13:39 -0500

    implemented main menu skeleton (note: the buttons don't do anything yet)

---
+++ Assets/Scripts/MenuRotateTurret.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuRotateTurret : MonoBehaviour
+{
+    [SerializeField] float turnSpeed = 10;
+    [SerializeField] float turnLimit = 45;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+
+        if(transform.rotation.y >= turnLimit || transform.rotation.y < 0)
+        {
+            turnSpeed *= -1;
+        }
+
+    }
+}
