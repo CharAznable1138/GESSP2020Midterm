15990ec6f67cd6e29e48e24f70378d0749a5ca9b
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-03-15 16:17:48 -0500

    Implemented functional main menu
    Unlike the previous commit, now the Play button starts the game, the Credits button displays credits (so far, this will need to be regularly updated later as more assets are implemented), and the Exit button exits

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject credits;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit()
+        #endif
+        //Note to self: Got this exit code from this website: https://answers.unity.com/questions/161858/startstop-playmode-from-editor-script.html
+        //Must double-check if it actually works
+    }
+
+    public void MainMenu()
+    {
+        credits.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void Credits()
+    {
+        mainMenu.SetActive(false);
+        credits.SetActive(true);
+    }
+
+}
