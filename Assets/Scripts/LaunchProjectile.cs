5ca092ab00fd278b40c8ce7555e39636d17d7667
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-03-20 17:24:14 -0500

    Implemented firing noise for player tank's cannon

--- Assets/Scripts/LaunchProjectile.cs
+++ Assets/Scripts/LaunchProjectile.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class LaunchProjectile : MonoBehaviour
 {
     [SerializeField] GameObject bulletPrefab;
+    private AudioSource firingNoise;
     // Start is called before the first frame update
     void Start()
     {
-        
+        firingNoise = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -16,6 +17,7 @@ public class LaunchProjectile : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
+            firingNoise.Play();
             Instantiate(bulletPrefab, new Vector3 (transform.position.x, transform.position.y, transform.position.z), new Quaternion (bulletPrefab.transform.rotation.x, transform.rotation.y, bulletPrefab.transform.rotation.z, bulletPrefab.transform.rotation.w));
         }
         
