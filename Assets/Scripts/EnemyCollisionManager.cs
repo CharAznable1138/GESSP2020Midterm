9b565421cd00d6a1faf1496cfee079624f3e40dd
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-03-24 18:51:59 -0500

    Added explosion particle effect for enemies
    Note: Must add sounds for explosions

--- Assets/Scripts/EnemyCollisionManager.cs
+++ Assets/Scripts/EnemyCollisionManager.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class EnemyCollisionManager : MonoBehaviour
 {
+    [SerializeField] GameObject explosion;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -20,6 +20,7 @@ public class EnemyCollisionManager : MonoBehaviour
     {
         if(other.gameObject.CompareTag("PlayerProjectile"))
         {
+            Instantiate(explosion, gameObject.transform.position, explosion.transform.rotation);
             Destroy(gameObject);
             Destroy(other.gameObject);
         }
---
+++ Assets/Scripts/ExplosionManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionManager : MonoBehaviour
+{
+    private ParticleSystem particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+        Destroy(gameObject, particles.main.duration);
+    }
+}
