51933e9a3afdf22a839c17ce8224a6fdc1e14577
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-04-18 19:20:37 -0500

    implemented coroutine for EnemyLaunchProjectile script
    enemies now launch bullets at random intervals between 1 and 1.5 seconds (not hard coded, can be easily changed from Inspector)

--- Assets/Scripts/EnemyLaunchProjectile.cs
+++ Assets/Scripts/EnemyLaunchProjectile.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class EnemyLaunchProjectile : MonoBehaviour
 {
     [SerializeField] GameObject bulletPrefab;
-    [SerializeField] float startDelay = 1;
-    [SerializeField] float repeatRate = 1;
+    [SerializeField] float minRepeatTime = 0.5f;
+    [SerializeField] float maxRepeatTime = 1.5f;
     private AudioSource firingNoise;
 
     // Start is called before the first frame update
     void Start()
     {
         firingNoise = GetComponent<AudioSource>();
-        InvokeRepeating("LaunchProjectile", startDelay, repeatRate);
+        StartCoroutine("LaunchProjectile");
     }
 
     // Update is called once per frame
@@ -22,9 +22,15 @@ public class EnemyLaunchProjectile : MonoBehaviour
         
     }
 
-    void LaunchProjectile()
+    IEnumerator LaunchProjectile()
     {
-        firingNoise.Play();
-        Instantiate(bulletPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+        while (true)
+        {
+            float waitTime = Random.Range(minRepeatTime, maxRepeatTime);
+            yield return new WaitForSeconds(waitTime);
+            firingNoise.Play();
+            Instantiate(bulletPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+            Debug.Log($"Enemy has fired projectile after waiting {waitTime} seconds.");
+        }
     }
 }
